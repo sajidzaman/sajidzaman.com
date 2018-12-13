dfcb73a3e55d6d64147d2d5c5151e0c834ff4e4b
Sajid Zaman <sajid.zaman@zeropoint.it>
2018-12-13 19:20:26 +0500

    updates seo meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import "../static/css/main.css";
 import MasonryLayout from "react-masonry-layout";
@@ -15,12 +16,45 @@ import {
   Container
 } from "semantic-ui-react";
 import Link from "next/link";
+import Head from "next/head";
 const masonryOptions = {
   transitionDuration: 0
 };
 const Index = () => {
   //console.log(sites);
   return (
+    <React.Fragment>
+    <Head>
+       <meta charset="utf-8" />    
+       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Sajid Badi-uz-zaman - sajidzaman.com" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222" />
+    <meta name="theme-color" content="#222222" />
+    <title> Sajid Badi-uz-zaman - Experienced Web Developer - PHP Development, Laravel Develpment, ReactJS Development - sajidzaman.com</title>
+<meta name="description" content="Development in PHP, Laravel, CakePHP, Javascript, React, React Native, Next.js, VueJS2, MySQL, Elasticsearch, MongoDB, Docker, Amazon Web Services (S3, EC2), Redis " />
+<meta itemprop="name" content="Sajid Badi-uz-zaman - Experienced Web Developer - PHP Development, Laravel Develpment, ReactJS Development - sajidzaman.com" />
+<meta itemprop="description" content="Development in PHP, Laravel, CakePHP, Javascript, React, React Native, Next.js, VueJS2, MySQL, Elasticsearch, MongoDB, Docker, Amazon Web Services (S3, EC2), Redis " />
+<meta itemprop="image" content="http://sajidzaman.com/static/images/sajid.zaman.png" />
+<meta name="twitter:card" content="Sajid Badi-uz-zaman - Experienced Web Developer - PHP Development, Laravel Develpment, ReactJS Development - sajidzaman.com" />
+<meta name="twitter:site" content="sajidzaman.com" />
+<meta name="twitter:title" content="Sajid Badi-uz-zaman - Experienced Web Developer - PHP Development, Laravel Develpment, ReactJS Development - sajidzaman.com" />
+<meta name="twitter:description" content="Development in PHP, Laravel, CakePHP, Javascript, React, React Native, Next.js, VueJS2, MySQL, Elasticsearch, MongoDB, Docker, Amazon Web Services (S3, EC2), Redis " />
+<meta name="twitter:creator" content="Sajid Badi-uz-zaman" />
+<meta name="twitter:image:src" content="http://sajidzaman.com/static/images/sajid.zaman.png" />
+<meta property="og:title" content="Sajid Badi-uz-zaman - Experienced Web Developer - PHP Development, Laravel Develpment, ReactJS Development - sajidzaman.com" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="http://www.sajidzaman.com" />
+<meta property="og:image" content="http://sajidzaman.com/static/images/sajid.zaman.png" />
+<meta property="og:description" content="Discover nearby museums and galleries, Access copyright free Images - No Charge, Buy & Sell Art - No Commission Fee" />
+<meta property="og:site_name" content="sajidzaman.com" />
+<link href="/favicon.ico" type="image/x-icon" rel="icon" />
+<link href="/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    </Head>
     <Container style={{ marginTop: 50 }}>
       <Grid>
         <GridRow className="border-shadow">
@@ -108,6 +142,7 @@ const Index = () => {
         </GridRow>
       </Grid>
     </Container>
+    </React.Fragment>
   );
 };
 
